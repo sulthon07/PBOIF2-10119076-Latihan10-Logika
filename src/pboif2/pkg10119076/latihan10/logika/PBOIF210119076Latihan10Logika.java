44644401873a528ecc424da4a45376df8f737d6a
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 17:33:37 +0700

    PBOIF2-10119076-Latihan10-Logika

---
+++ src/pboif2/pkg10119076/latihan10/logika/PBOIF210119076Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan10.logika;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : Logika
+ */
+public class PBOIF210119076Latihan10Logika {
+
+    
+    public static void main(String[] args) {
+       int umur = 19;
+       boolean dewasa;
+       dewasa = (umur >= 17);
+       System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
